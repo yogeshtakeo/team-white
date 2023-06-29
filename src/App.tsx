836bf3bb68547677b17779f570a6b24df88e83e5
uniquemozilla22 <uniquegaming26@gmail.com>
2023-06-28 23:37:27 -0400

    This is the homepage file you should commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import Homepage from "./components/Homepage.comp";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -9,6 +8,7 @@ function App() {
   return (
     <>
       <h1> Welcome </h1>
+      <Homepage />
     </>
   );
 }
