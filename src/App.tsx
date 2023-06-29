256ec16c401f95292e2ac148905379925de1afe8
Kabina Thapa <thapakabina28@gmail.com>
2023-06-29 16:39:36 -0400

    Install react-hot-toast

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+
 import "./App.css";
+import RegisterPage from "./pages/RegisterPage";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <h1> Welcome </h1>
+      <RegisterPage/>
     </>
   );
 }
--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import {Formik, Form} from 'formik'
+import {Formik, Form, Field} from 'formik'
+import * as Yup from 'yup'
 
 const RegisterPage = () => {
     const initialValues={
@@ -10,9 +11,30 @@ const RegisterPage = () => {
         password:'',
         confirmpassword:'',
     }
+    const validationSchema=Yup.object({
+        firstname:Yup.string().required('Enter First Name'),
+        lastname:Yup.string().required('Enter Last Name'),
+        dob:Yup.string().required('Enter dob'),
+        username:Yup.string().required('Enter Username'),
+        password:Yup.string().required('Enter Password'),
+        confirmpassword:Yup.string().required('Please Confirm Password'),
+    })
+    const submithandler=()=>{
+        toast.error('Succesfully created your account')
+    }
   return (
-    
+    <Formik 
+    initialValues={initialValues}
+    onSubmit={submithandler}
+    validationschema={validationSchema}>
+    <Form>
+        <Field type='text'
+            name='firstname'
+            label='First Name'/>
+            
 
+    </Form>
+    </Formik>
     
   )
 }
