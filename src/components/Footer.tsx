46d6ee927a46d8a0411bab97866695e691efc260
JunuThapa <junuthapa011@gmail.com>
2023-07-01 21:53:46 -0400

    Fixed the height of the Footer component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,8 +3,8 @@ import {AiFillLock} from 'react-icons/ai'
 
 function Footer() {
   return (
-    <div className="navbar flex flex-col justify-center align-center h-32 mt-6">
-      <div className="flex align-center justify-center"><AiFillLock/> Secure Area</div>
+    <div className="navbar flex flex-col justify-center align-center h-18 mt-6">
+      <div className="flex align-center justify-center pt-2"><AiFillLock/> Secure Area</div>
       <div className="flex justify-around align-center underline">
         <a href="#">Privacy</a>
         <a href="#">Security</a>
@@ -12,7 +12,7 @@ function Footer() {
         <a href="#">Legal Info & Disclosures</a>
         <a href="#">Equal Housing Lenders</a>
       </div>
-      <div className="flex align-center justify-center pt-7"> PayCircle,N.A.Member FDIC</div>
+      <div className="flex align-center justify-center pt-2"> PayCircle,N.A.Member FDIC</div>
       <div className="flex align-center justify-center">@ 2023 PayCircle Corporation</div>
     </div>
   );
