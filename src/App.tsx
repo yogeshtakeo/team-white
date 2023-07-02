84a19ca0c5727ee8db2edec723ff32ceb0b735a0
Namkhang Tsamchoe <ntnamkhang@gmail.com>
2023-07-01 20:51:14 -0400

    fix a minor error in App.tss file.

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,24 @@
 import "./App.css";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
 import HomePage from "./pages/HomePage";
 import RegisterPage from "./pages/RegisterPage";
 import LoginPage from "./pages/LoginPage";
-import {Routes, Route} from "react-router-dom"
+import { Routes, Route } from "react-router-dom";
 
 function App() {
- 
-
   return (
     <>
-    <Routes>
-      <Route path="register" element={<RegisterPage/>}></Route>
-      <Route path="login" element={<LoginPage/>}></Route>
-      <Route path="home" element={<HomePage/>}></Route>
+      <Routes>
+        <Route path="register" element={<RegisterPage />}></Route>
+        <Route path="login" element={<LoginPage />}></Route>
+        <Route path="home" element={<HomePage />}></Route>
       </Routes>
 
-   
       {/* <RegisterPage /> */}
       {/* <HomePage /> */}
       {/* <LoginPage /> */}
       {/* <Navbar /> */}
-   </>
-  )
+    </>
+  );
 }
 
-export default App
+export default App;
