2bc7cdab7b0082a09b2ed9a63b4d8505523574e2
Kabina Thapa <thapakabina28@gmail.com>
2023-07-04 19:08:55 -0400

    New CSS Design

--- src/components/BLogo.tsx
+++ src/components/BLogo.tsx
@@ -4,12 +4,12 @@ const BLogo = () => {
   return (
     <div className='absolute top-[-0.5%] left-[10%]'>
     <svg width="50" height="50" viewBox="0 0 200 200">
-    <circle cx="100" cy="100" r="80" fill="transparent" stroke="#000" strokeWidth="4" />
+    <circle cx="100" cy="100" r="80" fill="transparent" stroke="white" strokeWidth="5" />
    
         
       </svg>
       <svg width="48" height="48" viewBox="0 0 200 200" className='absolute top-[8%]'>
-    <circle cx="100" cy="100" r="80" fill="transparent" stroke="#000" strokeWidth="3" />
+    <circle cx="100" cy="100" r="80" fill="transparent" stroke="white" strokeWidth="6" />
    
         
       </svg>
